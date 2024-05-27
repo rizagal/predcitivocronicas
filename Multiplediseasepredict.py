13126cc9467df3ffb5531795cfae46749516a104
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-05-27 05:10:41 -0500

    Update Multiplediseasepredict.py
    ver

--- Multiplediseasepredict.py
+++ Multiplediseasepredict.py
@@ -12,7 +12,7 @@ from streamlit_option_menu import option_menu
 
 #Loading the saved models
 
-diabetes_model = pickle.load(open('diabetes_model.sav'))
+diabetes_model = pickle.load(open('/diabetes_model.sav','rb'))
 
 heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
 
