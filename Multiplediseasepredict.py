8007b545c05b70375d8ede0beedfe6ad1e536c88
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-06-02 08:50:22 -0500

    Update Multiplediseasepredict.py
    verd

--- Multiplediseasepredict.py
+++ Multiplediseasepredict.py
@@ -28,6 +28,14 @@ parkinsons_model = pickle.load(open('parkinsons_model.sav','rb'))
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Resultado Indicadores y Sistema de predicción de enfermedades", page_icon=":bar_chart:", layout="wide")
 
+#remove default theme
+theme_plotly = None # None or streamlit
+
+ 
+# CSS Style
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+
 st.sidebar.image("SIGES17.png",caption="")
 
 
