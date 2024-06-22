b66b7b3dbd3d90f5859c891916ca3111fa433af9
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-06-22 06:04:21 -0500

    Update Multiplediseasepredict.py

--- Multiplediseasepredict.py
+++ Multiplediseasepredict.py
@@ -12,7 +12,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.ensemble import IsolationForest
+
 import pandas as pd
 from pandasai import SmartDataframe
 import plotly.express as px  # pip install plotly-express
