aff5f1111e781f3188a47ab54b9aba77defcd5d9
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-06-22 06:31:44 -0500

    Update Multiplediseasepredict.py

--- Multiplediseasepredict.py
+++ Multiplediseasepredict.py
@@ -21,7 +21,7 @@ from pygwalker.api.streamlit import StreamlitRenderer
 
 
 # Este modelo lo genere en google colab en la cuenta de facildiez@gmail.com el archivo se llama Entrenar Modelo.ipynb, para crearlo me guie con: https://www.youtube.com/watch?v=lK0aVny0Rsw
-
+riesgocardio_model = pickle.load(open('model_datosderiesgo.pkl','rb'))
 
 diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
 
