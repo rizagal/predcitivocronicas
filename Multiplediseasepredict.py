efd540145bea626e53e46dc59559ec671bf861ff
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-07-23 15:08:34 -0500

    Update Multiplediseasepredict.py

--- Multiplediseasepredict.py
+++ Multiplediseasepredict.py
@@ -75,7 +75,7 @@ with st.sidebar:
                             'Deteccion de Datos Anomalos'
                             
                             ],
-                           icons = ['activity','pen','pen','pen','building','universal-access','book','heart','clipboard','person'],
+                           icons = ['activity','bar-chart','pen','pen','building','universal-access','book','heart','clipboard','person'],
                            default_index = 0)
    
 
