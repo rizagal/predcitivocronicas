01b1deeeffb5c4df7356b4e6fff67628f116a1c9
RICARDO ZUÑIGA GALARZA <rizagal@hotmail.com>
2024-07-23 16:38:09 -0500

    Update Multiplediseasepredict.py

--- Multiplediseasepredict.py
+++ Multiplediseasepredict.py
@@ -422,7 +422,7 @@ if(selected == 'Consulta Resultado Indicadores Atencion al Usuario'):
        mes1 = st.multiselect(
        "Seleccione Mes:",
        options=dfpqrs["MES"].unique(),
-       default=dfpqrs["MES"].unique()
+       
        )
     
     
